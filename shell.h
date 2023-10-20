7283dc130d444e3c89a3d8cfec2b7b93147d6b46
Zerubabel095 <zerubabel095@gmail.com>
2023-10-20 10:33:04 -0700

    updated

--- shell.h
+++ shell.h
@@ -16,21 +16,26 @@ char *path_finder(void);
 void exit_shell(char **command_args, char *input);
 void buffer_free(char **buf);
 void _signal(int s);
-extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 void print_prompt(void);
 char **tokenize_input(char *input);
 int builtin_check(char **cmd, char *buf);
 char *get_path_command(char **path, char *command_args);
 char *add_path(char *path, char *command_args);
 int builtins(char **command_args, char *input);
-extern char **environ;
 void execute_command(char* cp, char** cmd);
 void environment(void);
+
+/*strings*/
 int _strlen(char *s);
 int _strcmp(const char *s1, const char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strdup(const char *s);
 char *_strchr(char *s, char c);
+
+/* environment */
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+extern char **environ;
+
 struct info {
   int exit_code;
   int line_number;
